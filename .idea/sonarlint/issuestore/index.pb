5eb43c8ed3c2cee8e3f359c32be78668476fc14e
so87 <so87@evansville.edu>
2018-08-30 09:55:41 -0400

    fixed the storage issue with postgresql. unable to mount to windows ntfs

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+@
+nginx/Dockerfile,a\4\a4864c0d475111e1ee9e1c2e72d7d0c5e558de8d
+E
+postgresql/Dockerfile,1\1\11fe7782aabb861a3c9fb7bd066d89fcff32eae6
+>
+src/Dockerfile,b\1\b1ac484b20625f94ee8fee74dce04369ea1760f1
+@
+nginx/nginx.conf,6\6\665698d0c16ad6d96892b8344874c01c8e00f8f7
+B
+docker-compose.yml,3\5\35b8c13cf2eb2a194eada000eb310d65aed53b2a
+J
+src/A_MainSite/settings.py,0\a\0a9df43edffb4b20d498dea186af082323ab4977
--- start-windows.bat
+++ start-windows.bat
@@ -1,6 +1,10 @@
+echo create volume for storage
+docker volume create --name postgresql -d local
+docker volume create --name postgresql-data -d local
+
 echo opening firewall
 netsh advfirewall firewall add rule name="Open for docker http" protocol=TCP dir=in localport=80 action=allow
 netsh advfirewall firewall add rule name="Open for docker https" protocol=TCP dir=in localport=8443 action=allow
 
 echo starting containers
-docker-compose up -d
+docker-compose up -d --build
