52e34f890a673c401645fbcda95f736908536d6b
Simon Owens <so87@evansville.edu>
2018-09-11 10:55:40 -0400

    port 80 to 8080

--- build-windows.bat
+++ build-windows.bat
@@ -3,7 +3,7 @@ docker volume create --name postgresql -d local
 docker volume create --name postgresql-data -d local
 
 echo opening firewall
-netsh advfirewall firewall add rule name="Open for docker http" protocol=TCP dir=in localport=80 action=allow
+netsh advfirewall firewall add rule name="Open for docker http" protocol=TCP dir=in localport=8080 action=allow
 netsh advfirewall firewall add rule name="Open for docker https" protocol=TCP dir=in localport=8443 action=allow
 
 echo starting containers
