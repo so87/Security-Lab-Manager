f6d56b0c1c17822a801cc52f1f52d91050f902a0
so87 <so87@evansville.edu>
2018-09-19 22:12:59 -0400

    fixed absolute path in the secret text file

--- src/A_MainSite/settings.py
+++ src/A_MainSite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/code/A_MainSite/secret_key.txt') as f:
+with open('A_MainSite/secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
